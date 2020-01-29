b89081c7da40d240abb718275868da5474b5fa6e
lfmundim <lucas.mundim044@gmail.com>
2020-01-29 15:26:55 -0300

    add full path on health endpoint

--- content/Api/Blip.Api.Template/Startup.cs
+++ content/Api/Blip.Api.Template/Startup.cs
@@ -30,7 +30,7 @@ namespace Blip.Api.Template
         private const string SWAGGERFILE_PATH = "./swagger/v1/swagger.json";
         private const string API_VERSION = "v1";
         private const string SETTINGS_SECTION = "Settings";
-        private const string HEALTH_CHECK_ENDPOINT = "/health";
+        private const string HEALTH_CHECK_ENDPOINT = "http://localhost:80/health";
         private const string BLIP_CSS = "blip.css";
         private const string API_CHECK_KEY = "API";
 
